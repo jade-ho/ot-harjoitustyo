1e5a440b52f9d05bd130e42e3b2b51db4b791f8b
Jade Ho <jadeho@helsinki.fi>
2026-03-24 16:18:57 +0200

    Viikon 2 laskarit

--- laskarit/viikko2/maksukortti/src/maksukortti.py
+++ laskarit/viikko2/maksukortti/src/maksukortti.py
@@ -3,6 +3,10 @@ MAUKAS = 400
 
 
 class Maksukortti:
+
+    def saldo_euroina(self):
+        return self.saldo / 100
+    
     def __init__(self, saldo):
         # saldo on senteissä
         self.saldo = saldo
