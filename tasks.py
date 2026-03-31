359f460e2ef273e04c74d1cc2301bb702c0e73c8
Jade Ho <jadeho@helsinki.fi>
2026-03-31 16:57:21 +0300

    Add tasks.py file

---
+++ tasks.py
@@ -0,0 +1,23 @@
+# otettu mallia referenssisovelluksesta
+
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+@task
+def build(ctx):
+    ctx.run("python3 src/build.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
